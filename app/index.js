761848db0ef6db8a31db1e4d2144e5f903983a74
Gondwana Software <gondwanasoft@users.noreply.github.com>
2021-09-09 06:45:52 +1000

    Version that works on watch

--- app/index.js
+++ app/index.js
@@ -2,8 +2,37 @@ import document from 'document';
 import './widgets/canvas';
 
 const myCanvasEl = document.getElementById('myCanvas');
-myCanvasEl.fillPixel(150,100);
 
+/*// Speed test:
+myCanvasEl.autoRedraw = false;
+const PX_COUNT = 10000
+const tStart = Date.now()
+for (let i=0; i<PX_COUNT; i++) {
+  myCanvasEl.fillPixel(Math.random()*300,Math.random()*200);
+}
+console.log(`px/sec = ${PX_COUNT/(Date.now()-tStart)*1000}`)
+myCanvasEl.redraw();*/
+
+/*myCanvasEl.autoRedraw = false;
+myCanvasEl.fillStyle = 'red';
+myCanvasEl.fillPixel(12,9);
+myCanvasEl.fillPixel(1,0);
+myCanvasEl.fillPixel(2,0);
+myCanvasEl.fillPixel(3,0);
+myCanvasEl.fillStyle = '#00FF00';
+myCanvasEl.fillPixel(0,1);
+myCanvasEl.fillPixel(1,1);
+myCanvasEl.fillPixel(2,1);
+myCanvasEl.fillPixel(3,1);
+myCanvasEl.fillStyle = '#0000FF';
+myCanvasEl.globalAlpha = 0.5;
+myCanvasEl.fillPixel(0,2);
+myCanvasEl.fillPixel(1,2);
+myCanvasEl.fillPixel(2,2);
+myCanvasEl.fillPixel(3,2);
+myCanvasEl.redraw()*/
+
+myCanvasEl.autoRedraw = true;
 const touchEl = document.getElementById('touch');
 touchEl.onmousemove = e => {myCanvasEl.fillPixel(e.screenX-touchEl.x, e.screenY-touchEl.y);}
 
